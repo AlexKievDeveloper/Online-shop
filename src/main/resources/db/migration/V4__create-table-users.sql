dcfed7a0a571b099111d52fc44a2f656111e8da4
AlexKievDeveloper <glushkov18@gmail.com>
2020-09-29 12:42:23 +0300

    Removed alter table users from migration V4

--- src/main/resources/db/migration/V4__create-table-users.sql
+++ src/main/resources/db/migration/V4__create-table-users.sql
@@ -8,12 +8,3 @@ create table users
     password varchar(200) not null,
     role     role         not null
 );
-
-alter table users
-    owner to postgres;
-
-create unique index users_id_uindex
-    on users (id);
-
-create unique index users_login_uindex
-    on users (login);
